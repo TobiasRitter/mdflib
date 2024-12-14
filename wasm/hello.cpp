b4c40e11c5c04d8bf2632f30dd31bffa8d88b01e
Tobias Ritter <tobias.ritter98@gmail.com>
2024-12-14 18:07:08 +0000

    update

--- wasm/hello.cpp
+++ wasm/hello.cpp
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
 #include <emscripten/emscripten.h>
 
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
     int add(int a, int b) {
         return a + b;
+    }
+
+    EMSCRIPTEN_KEEPALIVE
+    int read(char* filename) {
+        std::ifstream f("file.txt");
+
+        if (f.is_open())
+            std::cout << f.rdbuf();
+
+        return 0;
     }
 }
