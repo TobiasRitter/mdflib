f1812393ffd681b071f7915fc3d1c0b20bd5a73a
Tobias Ritter <tobias.ritter98@gmail.com>
2024-12-14 19:25:05 +0000

    update

--- mdflib/main.cpp
+++ mdflib/main.cpp
@@ -9,21 +9,20 @@
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
 	int mf4read() {
-		CMdf4FileImport *pImport = new CMdf4FileImport;
+		CMdf4FileImport* pImport = new CMdf4FileImport;
 		if (pImport->ImportFile("./Test.mf4"))
 		{
-			// Display the content of the file
 			for (int i = 0; i < pImport->m_nDataGroups; i++)
 			{
 				CMdf4DataGroup *pGroup = pImport->m_vDataGroups[i];
 				pImport->DisplayGroup(pGroup);
 			}
-			std::cout << "Successfully read file\n";
 		}
 		else
 		{
 			std::cout << "Error reading file\n";
 		}
+		delete pImport;
 		return 0;
 	}
 }
